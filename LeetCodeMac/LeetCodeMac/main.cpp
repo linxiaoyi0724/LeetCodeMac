f9ab3b2abb6af9321793c0e326c1642b6f0060f3
林轶锽（Lin Yihuang）(linyihuang) <linyihuang@qiyi.com>
2021-10-30 14:15:53 +0800

    【Feature】 20211030

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -1390,3 +1390,253 @@ public:
 };*/
 
 
+
+
+
+
+
+
+/*
+//Task 22
+给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+
+ 
+
+说明:
+
+为什么返回数值是整数，但输出的答案是数组呢?
+
+请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
+
+你可以想象内部操作如下:
+
+// nums 是以“引用”方式传递的。也就是说，不对实参做任何拷贝
+int len = removeDuplicates(nums);
+
+// 在函数里修改输入数组对于调用者是可见的。
+// 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+ 
+示例 1：
+
+输入：nums = [1,1,2]
+输出：2, nums = [1,2]
+解释：函数应该返回新的长度 2 ，并且原数组 nums 的前两个元素被修改为 1, 2 。不需要考虑数组中超出新长度后面的元素。
+示例 2：
+
+输入：nums = [0,0,1,1,1,2,2,3,3,4]
+输出：5, nums = [0,1,2,3,4]
+解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+
+*/
+
+/*
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size() < 2){
+            return nums.size();
+        }
+        int j = 0;
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[j] != nums[i]){
+                nums[++j] = nums[i];
+            }
+        }
+        return ++j;
+    }
+};
+*/
+
+
+
+
+
+
+
+
+
+
+/*
+//Task 23
+编写一个函数来查找字符串数组中的最长公共前缀。
+
+如果不存在公共前缀，返回空字符串 ""。
+
+ 
+
+示例 1：
+
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+示例 2：
+
+输入：strs = ["dog","racecar","car"]
+输出：""
+解释：输入不存在公共前缀。
+
+*/
+
+/*
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string result = "";
+        if(strs.empty()){
+            return result;
+        }
+        
+        for(int i = 0; i < strs[0].size(); i++){
+            char temp = strs[0][i];
+            for(int j = 0; j < strs.size(); j++){
+                if(strs[j][i] == temp){
+                    continue;
+                }
+                else
+                {
+                    return result;
+                }
+            }
+            result += temp;
+        }
+        return  result;
+    }
+    
+};
+
+int main(){
+    Solution s;
+    vector<string> a = {"flower","flow","flight"};
+    std::cout << s.longestCommonPrefix(a) << std::endl;
+}
+*/
+
+
+
+
+/*
+//Task 24
+ 
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3
+
+*/
+
+/*
+#include <iostream>
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0),left(nullptr),right(nullptr){}
+};
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(!root){
+            return 0;
+        }
+        else{
+            int lChild = maxDepth(root->left);
+            int rChild = maxDepth(root->right);
+            return lChild > rChild ? lChild +1 : rChild +1;
+        }
+    }
+};
+*/
+
+
+
+
+
+
+/*
+//Task 25
+ 
+ 
+给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+
+高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+
+ 
+
+示例 1：
+
+
+输入：nums = [-10,-3,0,5,9]
+输出：[0,-3,9,-10,null,5]
+解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+
+示例 2：
+
+
+输入：nums = [1,3]
+输出：[3,1]
+解释：[1,3] 和 [3,1] 都是高度平衡二叉搜索树。
+
+*/
+
+
+/*
+#include <iostream>
+#include <vector>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode():val(0),left(nullptr),right(nullptr){}
+    TreeNode(int x):val(x),left(nullptr),right(nullptr){}
+};
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        int len = nums.size();
+        return  dfs(nums, 0, len);
+    }
+    TreeNode* dfs(vector<int>& nums, int p_left, int p_right){
+        if(p_left>=p_right){
+            return nullptr;
+        }
+        int mid = (p_left + p_right) / 2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = dfs(nums, p_left, mid);
+        root->right = dfs(nums, mid+1, p_right);
+        return root;
+    }
+};
+*/
+
+
+
+
+
+
+
+
+
