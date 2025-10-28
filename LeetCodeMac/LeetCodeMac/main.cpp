762479d220b46ef2fa5ff97ae1fd171e68436261
linxiaoyi <781210678@qq.com>
2025-10-28 23:00:10 +0800

    feat: 实现快乐数

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -3051,9 +3051,6 @@ int main(){
 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
 
 
-
- 
-
 示例 1:
 
 输入: rowIndex = 3
@@ -3075,8 +3072,7 @@ int main(){
 */
 
 
-
-
+/*
 #include<vector>
 #include<iostream>
 using namespace std;
@@ -3096,8 +3092,73 @@ public:
         return result;
     }
 };
+*/
+
+
+
+
+/*
+//Task 46
+ 
+ 编写一个算法来判断一个数 n 是不是快乐数。
+
+ 「快乐数」 定义为：
+
+ 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和。
+ 然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。
+ 如果这个过程 结果为 1，那么这个数就是快乐数。
+ 如果 n 是 快乐数 就返回 true ；不是，则返回 false 。
+
+  
+
+ 示例 1：
+
+ 输入：n = 19
+ 输出：true
+ 解释：
+ 1**2 + 9**2 = 82
+ 8**2 + 2**2 = 68
+ 6**2 + 8**2 = 100
+ 1**2 + 0**2 + 0**2 = 1
+ 示例 2：
+
+ 输入：n = 2
+ 输出：false
+ 
+*/
+
+
+#include <unordered_set>
+#include <iostream>
+using namespace std;
+class Solution{
+public:
+    int sumOfSquareNumber(int n){
+        int sum = 0;
+        while (n > 0) {
+            int heihestPos = n % 10;
+            sum += heihestPos *heihestPos;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n){
+        unordered_set<int> s;
+        s.insert(n);
+        while(n != 1){
+            n = sumOfSquareNumber(n);
+            if (s.find(n) != s.end()){
+                return false;
+            }
+            s.insert(n);
+        }
+        return true;
+    }
+};
+
 
 int main(){
-    std::cout << "hello" << std::endl;
-    std::cin.ignore();
+    Solution sl;
+    std::cout << sl.isHappy(19) << std::endl;
 }
