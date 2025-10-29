af732da5d58d22db43a4f7a15a9c84b331a9cc2d
linxiaoyi <781210678@qq.com>
2025-10-29 23:10:42 +0800

    feat: 实现单词切分

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -3125,7 +3125,7 @@ public:
  输入：n = 2
  输出：false
  
-*/
+
 
 
 #include <unordered_set>
@@ -3156,9 +3156,76 @@ public:
         return true;
     }
 };
+ */
 
 
+
+
+
+
+
+
+
+/*
+//Task 45 单词拆分
+中等
+相关标签
+premium lock icon
+相关企业
+给你一个字符串 s 和一个字符串列表 wordDict 作为字典。如果可以利用字典中出现的一个或多个单词拼接出 s 则返回 true。
+
+注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+
+ 
+
+示例 1：
+
+输入: s = "leetcode", wordDict = ["leet", "code"]
+输出: true
+解释: 返回 true 因为 "leetcode" 可以由 "leet" 和 "code" 拼接成。
+示例 2：
+
+输入: s = "applepenapple", wordDict = ["apple", "pen"]
+输出: true
+解释: 返回 true 因为 "applepenapple" 可以由 "apple" "pen" "apple" 拼接成。
+     注意，你可以重复使用字典中的单词。
+示例 3：
+
+输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+输出: false
+*/
+
+
+#include<string>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordDictSet;
+        for(auto word :wordDict){
+            wordDictSet.insert(word);
+        }
+        
+        auto dp = vector<bool>(s.size()+1);
+        dp[0] = true;
+        for(int i = 1; i <= s.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(dp[j] && wordDictSet.find(s.substr(j, i-j)) != wordDictSet.end()){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.size()];
+    }
+};
 int main(){
-    Solution sl;
-    std::cout << sl.isHappy(19) << std::endl;
+    Solution sq;
+    string s = "leetcode";
+    vector<string> wordDict = {"leet", "code"};
+    return sq.wordBreak(s, wordDict);
 }
+
+
