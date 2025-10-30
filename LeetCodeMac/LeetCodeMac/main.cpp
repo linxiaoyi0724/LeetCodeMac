4930b296a9186201cc9116f8f59f77b37e8b52ca
linxiaoyi <781210678@qq.com>
2025-10-30 22:48:29 +0800

    feat: 存在重复元素

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -3195,7 +3195,7 @@ premium lock icon
 输出: false
 */
 
-
+/*
 #include<string>
 #include<vector>
 #include<unordered_set>
@@ -3228,4 +3228,51 @@ int main(){
     return sq.wordBreak(s, wordDict);
 }
 
+*/
 
+
+
+
+
+
+/*
+//Task 46 存在重复元素 II
+ 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+
+  
+
+ 示例 1：
+
+ 输入：nums = [1,2,3,1], k = 3
+ 输出：true
+ 示例 2：
+
+ 输入：nums = [1,0,1,1], k = 1
+ 输出：true
+ 示例 3：
+
+ 输入：nums = [1,2,3,1,2,3], k = 2
+ 输出：false
+*/
+
+
+#include<vector>
+#include<unordered_set>
+#include<cmath>
+using namespace std;
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> s;
+        for(int i = 0; i<nums.size(); i++){
+            if(i > k){
+                s.erase(nums[i-k-1]);
+            }
+            if(s.count(nums[i])){
+                return true;
+            }
+            s.emplace(nums[i]);
+        }
+        return false;
+    }
+};
