ef419b3faecbf19b40478f06a6a2407fe873b3ae
linxiaoyi <781210678@qq.com>
2025-11-02 10:42:35 +0800

    feat: 环形链表

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -3310,7 +3310,7 @@ public:
  */
 
 
-
+/*
 #include <vector>
 #include <unordered_set>
 #include <algorithm>
@@ -3344,3 +3344,63 @@ int main(){
     Solution sl;
     std::cout << sl.longestConsecutive(nums) << std::endl;
 }
+*/
+
+
+
+
+
+
+
+
+/*
+ //Task 48 环形链表
+ 
+ 给你一个链表的头节点 head ，判断链表中是否有环。
+
+ 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+
+ 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+
+ */
+
+#include <unordered_set>
+#include <vector>
+#include <iostream>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode(int x):val(x),next(NULL){}
+};
+
+class Solution{
+public:
+    bool hasCycle(ListNode* head){
+        unordered_set<ListNode*> node_set;
+        while (head) {
+            if(node_set.count(head)){
+                return true;
+            }
+            node_set.insert(head);
+            head = head->next;
+        }
+        return false;
+    }
+};
+
+int main(){
+    ListNode* head = new ListNode(3);
+    ListNode* n2 = new ListNode(2);
+    ListNode* n3 = new ListNode(0);
+    ListNode* n4 = new ListNode(-4);
+    
+    head->next = n2;
+    n2->next = n3;
+    n3->next = n4;
+    n4->next = n2;
+    
+    Solution sl;
+    std::cout << sl.hasCycle(head) << std::endl;
+}
+
