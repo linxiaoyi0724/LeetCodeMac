04f9f0507cdfc805806c1a33ae575de42d92abc1
linxiaoyi <781210678@qq.com>
2025-11-05 23:52:06 +0800

    feat: 最小栈

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -3708,7 +3708,7 @@ int main(){
 
 
 
-
+/*
 #include <vector>
 #include <string>
 #include <iostream>
@@ -3765,3 +3765,93 @@ int main(){
     Solution sl;
     std::cout << sl.simplifyPath(path) << std::endl;
 }
+*/
+
+
+
+
+/*
+ //Task 53 最小栈
+ 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+ 实现 MinStack 类:
+
+ MinStack() 初始化堆栈对象。
+ void push(int val) 将元素val推入堆栈。
+ void pop() 删除堆栈顶部的元素。
+ int top() 获取堆栈顶部的元素。
+ int getMin() 获取堆栈中的最小元素。
+  
+
+ 示例 1:
+
+ 输入：
+ ["MinStack","push","push","push","getMin","pop","top","getMin"]
+ [[],[-2],[0],[-3],[],[],[],[]]
+
+ 输出：
+ [null,null,null,null,-3,null,0,-2]
+
+ 解释：
+ MinStack minStack = new MinStack();
+ minStack.push(-2);
+ minStack.push(0);
+ minStack.push(-3);
+ minStack.getMin();   --> 返回 -3.
+ minStack.pop();
+ minStack.top();      --> 返回 0.
+ minStack.getMin();   --> 返回 -2.
+  
+
+ 提示：
+
+ -231 <= val <= 231 - 1
+ pop、top 和 getMin 操作总是在 非空栈 上调用
+ push, pop, top, and getMin最多被调用 3 * 104 次
+
+ */
+
+
+#include <stack>
+#include <iostream>
+using namespace std;
+class MinStack{
+public:
+    stack<int> x_stack;
+    stack<int> min_stack;
+public:
+    MinStack(){
+        min_stack.push(INT_MAX);
+    }
+    
+    void push(int val){
+        x_stack.push(val);
+        min_stack.push(std::min(min_stack.top(),val));
+    }
+    
+    void pop(){
+        x_stack.pop();
+        min_stack.pop();
+    }
+    
+    int getMin(){
+        return min_stack.top();
+    }
+    
+    int top(){
+        return x_stack.top();
+    }
+
+};
+
+int main(){
+    MinStack minStack;
+    minStack.push(-2);
+    minStack.push(0);
+    minStack.push(-3);
+    std::cout << minStack.getMin() << std::endl;
+    minStack.pop();
+    std::cout << minStack.top() << std::endl;
+    std::cout << minStack.getMin() << std::endl;
+}
+
