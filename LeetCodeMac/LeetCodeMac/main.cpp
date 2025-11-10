ce7041b9cf99ba204e267262c5d9f289ddde91bc
linxiaoyi <781210678@qq.com>
2025-11-10 23:42:22 +0800

    feat: 合并两个有序链表

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -4184,6 +4184,7 @@ int main(){
  */
 
 
+/*
 #include <vector>
 #include <iostream>
 using namespace std;
@@ -4247,4 +4248,93 @@ int main(){
     for(int i = result.size()-1; i>=0; i--){
         std::cout << result[i] << std::endl;
     }
+}
+*/
+
+
+
+
+
+/*
+ //Task 58 合并两个有序链表
+ 
+ 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+
+  
+
+ 示例 1：
+
+
+ 输入：l1 = [1,2,4], l2 = [1,3,4]
+ 输出：[1,1,2,3,4,4]
+ 示例 2：
+
+ 输入：l1 = [], l2 = []
+ 输出：[]
+ 示例 3：
+
+ 输入：l1 = [], l2 = [0]
+ 输出：[0]
+  
+
+ 提示：
+
+ 两个链表的节点数目范围是 [0, 50]
+ -100 <= Node.val <= 100
+ l1 和 l2 均按 非递减顺序 排列
+*/
+
+
+#include <vector>
+#include <iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(): val(0),next(nullptr){}
+    ListNode(int x): val(x), next(nullptr){}
+    ListNode(int x, ListNode* next): val(x), next(next){}
+};
+
+class Solution{
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+        if(!list1){
+            return list2;
+        }
+        if(!list2){
+            return list1;
+        }
+        
+        if(list1->val < list2->val){
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }else{
+            list2->next = mergeTwoLists(list2->next, list1);
+            return list2;
+        }
+    }
+};
+
+int main(){
+    ListNode* l1 = new ListNode(1);
+    ListNode* l1_2 = new ListNode(2);
+    ListNode* l1_3 = new ListNode(4);
+    l1->next = l1_2;
+    l1_2->next = l1_3;
+    
+    ListNode* l2 = new ListNode(1);
+    ListNode* l2_2 = new ListNode(3);
+    ListNode* l2_3 = new ListNode(4);
+    l2->next = l2_2;
+    l2_2->next = l2_3;
+    
+    Solution sl;
+    ListNode* L3 = sl.mergeTwoLists(l1, l2);
+    
+    while (L3) {
+        std::cout << L3->val << std::endl;
+        L3 = L3->next;
+    }
+    
 }
