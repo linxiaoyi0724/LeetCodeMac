19aeea722f15f11e46b3e8149f6fed50de7f86e3
linxiaoyi <781210678@qq.com>
2025-12-11 01:14:15 +0800

    feat: 分隔链表

--- LeetCodeMac/LeetCodeMac/main.cpp
+++ LeetCodeMac/LeetCodeMac/main.cpp
@@ -4858,6 +4858,7 @@ int main(){
 */
 
 
+/*
 #include <iostream>
 #include <math.h>
 using namespace std;
@@ -4900,9 +4901,6 @@ public:
     }
 };
 
-//输入：head = [1,2,3,4,5], k = 2
-//输出：[4,5,1,2,3]
-//示例 2：
 
 int main(){
     ListNode* head = new ListNode(0);
@@ -4923,4 +4921,93 @@ int main(){
         std::cout << headResult->val << std::endl;
         headResult = headResult->next;
     }
+}
+*/
+
+
+
+
+
+
+
+/*
+ //Task 65 分隔链表
+ 
+ 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前。
+ 
+ 你应当 保留 两个分区中每个节点的初始相对位置。
+ 
+ 
+ 
+ 示例 1：
+ 
+ 
+ 输入：head = [1,4,3,2,5,2], x = 3
+ 输出：[1,2,2,4,3,5]
+ 示例 2：
+ 
+ 输入：head = [2,1], x = 2
+ 输出：[1,2]
+ */
+
+#include <iostream>
+using namespace std;
+struct ListNode{
+    int val;
+    ListNode* next;
+    ListNode():val(0),next(nullptr){}
+    ListNode(int val): val(val),next(nullptr){}
+    ListNode(int val, ListNode* next):val(val),next(next){}
+};
+
+class Solution{
+public:
+    ListNode* partition(ListNode* head, int x){
+        ListNode* smallHead = new ListNode(0);
+        ListNode* small = smallHead;
+        ListNode* largeHead = new ListNode(0);
+        ListNode* large = largeHead;
+        
+        while(head){
+            if(head->val < x){
+                small->next = head;
+                small = small->next;
+            }else{
+                large->next = head;
+                large = large->next;
+            }
+            head = head->next;
+        }
+        
+        large->next = nullptr;
+        small->next = largeHead->next;
+        return smallHead->next;
+    }
+};
+
+//
+//输入：head = [1,4,3,2,5,2], x = 3
+//输出：[1,2,2,4,3,5]
+//示例 2：
+int main(){
+    ListNode* head = new ListNode(1);
+    ListNode* head_2 = new ListNode(4);
+    ListNode* head_3 = new ListNode(3);
+    ListNode* head_4 = new ListNode(2);
+    ListNode* head_5 = new ListNode(5);
+    ListNode* head_6 = new ListNode(2);
+    
+    head->next = head_2;
+    head_2->next = head_3;
+    head_3->next = head_4;
+    head_4->next = head_5;
+    head_5->next = head_6;
+    
+    Solution sl;
+    ListNode* resultHead = sl.partition(head, 3);
+    while (resultHead) {
+        std::cout << resultHead->val <<std::endl;
+        resultHead = resultHead->next;
+    }
+    
 }
